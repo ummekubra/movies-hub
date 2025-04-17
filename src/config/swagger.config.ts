94d0baf55bfa413ca226814e4bd10b8a73780341
ummekubra <umme.kubra93@gmail.com>
2025-04-17 23:08:22 +0300

    swagger integration

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Movies API')
+    .setDescription('API documentation for the movies')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT ?? 3000);
+  // Enable CORS for global use
+  app.enableCors();
+  // Set up Swagger using the standalone config
+
+  // Apply ValidationPipe globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Automatically strip non-whitelisted properties
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are found
+      transform: true, // Transform payloads into DTO instances
+    }),
+  );
+
+  setupSwagger(app);
+  await app.listen(process.env.SERVER_PORT ?? 8080);
 }
 bootstrap();
